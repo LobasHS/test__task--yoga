433956457b3ff77a11c8662d792caefceecf2e48
LobasHS <lobas.hennadiy.com>
2017-10-06 12:59:24 +0300

    js price buutom hover

---
+++ src/js/price.js
@@ -0,0 +1,24 @@
+$('#button__one').hover(
+    function(){
+        $('#header__one ').css('background-color','#475bf1');
+    },
+    function(){
+        $('#header__one ').css('background-color','');
+    }
+);
+$('#button__two').hover(
+    function(){
+        $('#header__two').css('background-color','#475bf1');
+    },
+    function(){
+        $('#header__two').css('background-color','');
+    }
+);
+$('#button__three').hover(
+    function(){
+        $('#header__three').css('background-color','#475bf1');
+    },
+    function(){
+        $('#header__three').css('background-color','');
+    }
+);
